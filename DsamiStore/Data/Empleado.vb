1c6bee88f5e3ccbc79866a032c3989c65175dfc0
Fernando Monja Vasquez <fernandomonjav@gmail.com>
2022-10-17 19:03:07 -0500

    Agregar paginacion y buscador a empleados

--- DsamiStore/Data/Empleado.vb
+++ DsamiStore/Data/Empleado.vb
@@ -1,4 +1,6 @@
-﻿Public Class Empleado
+﻿Imports System.Data.SqlClient
+
+Public Class Empleado
     Inherits Persona
     Private idEmpleado As String
 
@@ -10,9 +12,8 @@
     Private idDistrito As String
     Private idProvincia As String
 
-    Public Sub New()
-        MyBase.New()
-    End Sub
+    Private cmd As SqlCommand
+    Private dr As SqlDataReader
 
     Public Sub New(idEmpleado As String, direccion As String, tipoDocumento As String,
                    numeroDocumento As String, idRegion As String, idDistrito As String,
@@ -33,6 +34,12 @@
         Me.idProvincia = idProvincia
 
     End Sub
+
+
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
 
     Public Property Id_Empleado As String
         Get
@@ -98,4 +105,124 @@
             idProvincia = value
         End Set
     End Property
+
+    Public Function ConsultarEmpleado() As List(Of Empleado)
+        Try
+            Dim empleados As List(Of Empleado) = New List(Of Empleado)
+            Dim i As Integer = 0
+
+            conectar()
+
+            cmd = New SqlCommand("sp_empleado_listar")
+
+            cmd.Connection = con
+
+            dr = cmd.ExecuteReader
+
+            If dr.HasRows Then
+                While dr.Read
+                    i += 1
+                    empleados.Add(New Empleado(
+                                 dr.Item("i_id_empleado"),
+                                 dr.Item("v_direccion_ubicacion"),
+                                 dr.Item("v_nombre_tipo_documento"),
+                                 dr.Item("v_valor_documento"),
+                                 dr.Item("i_id_region"),
+                                 dr.Item("i_id_distrito"),
+                                 dr.Item("i_id_provincia"),
+                        dr.Item("i_id_persona"),
+                        dr.Item("v_nombre_persona"),
+                        dr.Item("v_apellido_paterno_persona"),
+                        dr.Item("v_apellido_materno_persona"),
+                        dr.Item("i_id_documento"),
+                        dr.Item("d_fecha_nacimiento_persona"),
+                        dr.Item("i_id_pais")
+                                   ))
+                End While
+
+                Return empleados
+            Else
+                Return Nothing
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message & " EMPLEADO")
+            Return Nothing
+        Finally
+            con.Close()
+        End Try
+    End Function
+
+    Public Function EncontrarEmpleadosCantidad(consulta As String, limite As Integer) As Integer
+        Try
+            Dim listClien As List(Of Roles)
+            listClien = New List(Of Roles)
+            Dim i As Integer = 0
+            conectar()
+            cmd = New SqlCommand("sp_empleado_listar_paginacion_count @consulta, @limite")
+            cmd.Parameters.AddWithValue("@consulta", consulta)
+            cmd.Parameters.AddWithValue("@limite", limite)
+
+            cmd.Connection = con
+
+            Return cmd.ExecuteScalar()
+
+        Catch ex As Exception
+            MsgBox(ex.Message & " EMPLEADO")
+            Return Nothing
+        Finally
+            con.Close()
+        End Try
+    End Function
+
+    Public Function EncontrarEmpleados(consulta As String, pagina As Integer, limite As Integer) As List(Of Empleado)
+        Try
+            Dim empleados As List(Of Empleado) = New List(Of Empleado)
+            Dim i As Integer = 0
+
+            conectar()
+
+            cmd = New SqlCommand("sp_empleado_listar_paginacion @consulta, @pagina, @limite")
+            cmd.Parameters.AddWithValue("@consulta", consulta)
+            cmd.Parameters.AddWithValue("@pagina", pagina)
+            cmd.Parameters.AddWithValue("@limite", limite)
+
+            cmd.Connection = con
+
+            dr = cmd.ExecuteReader
+
+            If dr.HasRows Then
+                While dr.Read
+                    i += 1
+                    empleados.Add(New Empleado(
+                                 dr.Item("i_id_empleado"),
+                                 dr.Item("v_direccion_ubicacion"),
+                                 dr.Item("v_nombre_tipo_documento"),
+                                 dr.Item("v_valor_documento"),
+                                 dr.Item("i_id_region"),
+                                 dr.Item("i_id_distrito"),
+                                 dr.Item("i_id_provincia"),
+                        dr.Item("i_id_persona"),
+                        dr.Item("v_nombre_persona"),
+                        dr.Item("v_apellido_paterno_persona"),
+                        dr.Item("v_apellido_materno_persona"),
+                        dr.Item("i_id_documento"),
+                        dr.Item("d_fecha_nacimiento_persona"),
+                        dr.Item("i_id_pais")
+                                   ))
+                End While
+
+                Return empleados
+            Else
+                Return Nothing
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message & " EMPLEADO")
+            Return Nothing
+        Finally
+            con.Close()
+        End Try
+    End Function
+
 End Class
--- DsamiStore/Presentation/FormEmpleados.vb
+++ DsamiStore/Presentation/FormEmpleados.vb
@@ -1,3 +1,106 @@
 ﻿Public Class FormEmpleados
+    Private _categoriaNegocio As BLEmpleado = New BLEmpleado()
+    Private _consulta As String = ""
+    Private _pagina As Integer = 1
+    Private _totalPagina As Integer = 1
+    Private _limite As Integer = 5
+    Private _dtLimite As New DataTable()
+
+    Private _empleadoNegocio As BLEmpleado = New BLEmpleado()
+
+    Public Sub New()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+
+        ' Establecer columnas valor y texto a la tabla
+        _dtLimite.Columns.Add("valor")
+        _dtLimite.Columns.Add("texto")
+
+        ' Agregar filas conn valor y texto
+        _dtLimite.Rows.Add(5, "5 registros")
+        _dtLimite.Rows.Add(10, "10 registros")
+        _dtLimite.Rows.Add(25, "25 registros")
+        _dtLimite.Rows.Add(50, "50 registros")
+
+        ' Establecer tabla al combobox
+        cbLimite.DataSource = _dtLimite
+
+        ' Decirle que columna será el valor del combobox
+        cbLimite.ValueMember = "valor"
+        ' Decirle que columna será el texto del comobobox
+        cbLimite.DisplayMember = "texto"
+
+        cbLimite.SelectedIndex = 0
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        tbPagina.Text = _pagina
+    End Sub
+
+    Private Sub FormEmpleados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Cargardatos()
+    End Sub
+
+    Public Sub Cargardatos()
+        Try
+            dgvEmpleados.Rows.Clear()
+
+            tbPagina.Text = _pagina
+
+            If _pagina = 1 Then
+                btnAnterior.Enabled = False
+            Else
+                btnAnterior.Enabled = True
+            End If
+
+            If _pagina = _totalPagina Then
+                btnSiguiente.Enabled = False
+            Else
+                btnSiguiente.Enabled = True
+            End If
+
+            _totalPagina = _empleadoNegocio.ObtenerEmpleadosCantidad(_consulta, _limite)
+
+            Dim empleados As List(Of Empleado) = _empleadoNegocio.ObtenerEmpleados(_consulta, _pagina, _limite)
+
+            Dim i As Integer = 1
+
+            For x = 0 To empleados.Count - 1 Step 1
+                dgvEmpleados.Rows.Add((x + 1).ToString(), empleados(x).Nombre,
+                                        empleados(x).ApellidoPaterno, empleados(x).ApellidoMaterno)
+            Next
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message & " FORM ROLES")
+        End Try
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbLimite.SelectedIndexChanged
+        ' Obtener valor del combobox
+        _limite = cbLimite.SelectedItem.Row.Item("valor")
+
+        Cargardatos()
+    End Sub
+
+    Private Sub btnAnteriorClicked(sender As Object, e As EventArgs) Handles btnAnterior.Click
+        _pagina -= 1
+
+        Cargardatos()
+    End Sub
+
+    Private Sub btnSiguienteClicked(sender As Object, e As EventArgs) Handles btnSiguiente.Click
+        _pagina += 1
+
+        Cargardatos()
+    End Sub
+
+    Private Sub btnBuscarClicked(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        _consulta = tbBuscar.Text
+
+        Cargardatos()
+    End Sub
 
 End Class
